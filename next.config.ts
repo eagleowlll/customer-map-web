b4405f6e3e2961085d42983d315b9aa230ad917b
eagleowlll <1894citizenwon@gmail.com>
2026-04-24 14:32:34 +0900

    next config수정

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.resolve.alias.canvas = false
-    return config
-  },
 };
 
 export default nextConfig;
